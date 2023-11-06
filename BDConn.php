a7b047d3a1c7dfc253c63fae22b205da97b7abcf
Roger <Roger@161.111.139.225>
2023-11-06 13:22:47 +0100

    Arreglo del nul dela fecha

--- BDConn.php
+++ BDConn.php
@@ -32,7 +32,7 @@ class BDConn {
     
     public function insertDBEvent($desc,$tipo,$timestamp,$pos,$profundidad,$temp_agua,$sal,$fluor,$conductividad,$temp_aire,$humedad,$pres_atmos,$vel_med_viento,$fecha_fin,$instrument) {
         $sql = "INSERT INTO eventos (Descripcion, Tipo, Timestamp, Pos, Profundidad, Temp_agua, Sal, Fluor, Conductividad, Temp_aire, Humedad, Pres_atmos, Vel_med_viento, Fecha_fin, Instrument)"
-                . "VALUES ('$desc','$tipo','$timestamp','$pos','$profundidad','$temp_agua','$sal','$fluor','$conductividad','$temp_aire','$humedad','$pres_atmos','$vel_med_viento','$fecha_fin','$instrument')";
+                . "VALUES ('$desc','$tipo','$timestamp','$pos','$profundidad','$temp_agua','$sal','$fluor','$conductividad','$temp_aire','$humedad','$pres_atmos','$vel_med_viento',".($fecha_fin=='null' ? $fecha_fin : "$fecha_fin").",'$instrument')";
         //DESCOMENTA ESTO PARA VER LA QUERY EN LA APP
         echo $sql;
         mysqli_query($this->conexion, $sql) or die(mysqli_error($this->conexion));
